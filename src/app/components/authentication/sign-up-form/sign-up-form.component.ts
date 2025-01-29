ebf9dddddc7c23742f0c185442d88d9704c88bce
Harsh Shah Jaiswal <tech.harsh2003@gmail.com>
2025-01-29 12:35:02 +0530

    otp verfication done

--- src/app/components/authentication/sign-up-form/sign-up-form.component.ts
+++ src/app/components/authentication/sign-up-form/sign-up-form.component.ts
@@ -3,6 +3,8 @@ import { SignUp } from './sign-up-form.interface';
 import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
 import { SignUpService } from '../../../services/auth/sign-up.service';
 
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+
 @Component({
   selector: 'app-sign-up-form',
   standalone: false,
@@ -14,6 +16,8 @@ export class SignUpFormComponent implements OnChanges {
   @Input() role : string = '';
   signUpForm!: FormGroup;
 
+  otpNumber : number = Math.floor(Math.random()*(99999-10001 + 1) + 10001);
+
   constructor(private fb : FormBuilder, private signUpService : SignUpService){
     this.save();
   }
@@ -21,10 +25,12 @@ export class SignUpFormComponent implements OnChanges {
   private save(){
     this.signUpForm = this.fb.group({
       name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      user: ['', Validators.required],
+      // user: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       cPassword: ['', Validators.required],
-      role: [this.role, Validators.required]
+      role: [this.role, Validators.required],
+      message : ['', Validators.required]
     })
   }
 
@@ -35,16 +41,48 @@ export class SignUpFormComponent implements OnChanges {
     }
   }
 
-  signUpSubmit(){
+  otp(e : Event){
+    e.preventDefault();
+    const templateParams = {
+      user: this.signUpForm.get('user')?.value,  
+      name: this.signUpForm.get('name')?.value,  
+      message: this.otpNumber.toString(),  
+    };
+
+    emailjs
+    .send('service_li70tn6', 'template_xaeme4r', templateParams, 'twow_OiDcodzo3LCz')                                                                
+      // .sendForm('service_li70tn6', 'template_xaeme4r', e.target as HTMLFormElement, {
+      //   publicKey: 'twow_OiDcodzo3LCz',                                                        
+      // })                                                                
+      .then(
+        () => {
+          alert("Email sent successfully")
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+    
+  }
+
+  signUpSubmit(e : Event){
+    console.log("Hello");
+    
     if(this.signUpForm.valid){
       const password = this.signUpForm.get('password')?.value;
       const cPassword = this.signUpForm.get('cPassword')?.value;
       if(password === cPassword){
-        const formValue = this.signUpForm.value as SignUp      
-        this.signUpService.createUser(formValue).subscribe(res => {
-          alert("Thankyou for joining skillify");
-          this.signUpForm.reset();
-        })
+        if (this.otpNumber === parseInt(this.signUpForm.get('message')?.value)) {
+          console.log("Noicceee");
+          const formValue = this.signUpForm.value as SignUp      
+          this.signUpService.createUser(formValue).subscribe(res => {
+            alert("Thankyou for joining skillify");
+            this.signUpForm.reset();
+          })
+        }else{
+          alert("OTP is incorrect")
+        }
       }else{
         alert("Password doesn't match")
       }
--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Contact } from './contact.interface';
 import { ContactService } from '../../services/contact.service';
 
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+
 @Component({
   selector: 'app-contact-form',
   standalone: false,
@@ -14,6 +16,7 @@ export class ContactFormComponent {
 
   constructor (private contactService: ContactService){}
 
+
   ngOnInit(){
     this.contact = {
       name: '',
@@ -22,11 +25,25 @@ export class ContactFormComponent {
     }
   }
 
-  save(model: Contact){
+  save(model: Contact, e : Event){
+    e.preventDefault();
     console.log(model);
     this.contactService.sendMessage(model).subscribe(res=>{
       console.log(res);
       
     })
+
+    emailjs
+      .sendForm('service_li70tn6', 'template_997o6xn', e.target as HTMLFormElement, {
+        publicKey: 'twow_OiDcodzo3LCz',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
   }
 }
